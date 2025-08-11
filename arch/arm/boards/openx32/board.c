cd667665f134d9364c7cd75edf1c0e027e0ff222
Alexander Schulz <code@schulzalex.de>
2025-08-11 23:27:18 +0200

    good color on LCD

--- arch/arm/boards/openx32/board.c
+++ arch/arm/boards/openx32/board.c
@@ -119,7 +119,7 @@ console_initcall(openx32_init);
 // ######################################################################
 
 
-static void tx25_fb_enable(int enable)
+static void openx32_fb_enable(int enable)
 {
 	if (enable) {
 		writel(0x000100A1, 0x53FC8000);
@@ -128,37 +128,92 @@ static void tx25_fb_enable(int enable)
 	}
 }
 
-static struct fb_videomode stk5_fb_mode = {
-	.name = "Innolux-AT070TN",
+static iomux_v3_cfg_t openx32_lcdc_gpios[] = {
+	MX25_PAD_LSCLK__LSCLK,
+	MX25_PAD_LD0__LD0,
+	MX25_PAD_LD1__LD1,
+	MX25_PAD_LD2__LD2,
+	MX25_PAD_LD3__LD3,
+	MX25_PAD_LD4__LD4,
+	MX25_PAD_LD5__LD5,
+	MX25_PAD_LD6__LD6,
+	MX25_PAD_LD7__LD7,
+	MX25_PAD_LD8__LD8,
+	MX25_PAD_LD9__LD9,
+	MX25_PAD_LD10__LD10,
+	MX25_PAD_LD11__LD11,
+	MX25_PAD_LD12__LD12,
+	MX25_PAD_LD13__LD13,
+	MX25_PAD_LD14__LD14,
+	MX25_PAD_LD15__LD15,
+	MX25_PAD_D15__LD16,
+	MX25_PAD_D14__LD17,
+	MX25_PAD_HSYNC__HSYNC,
+	MX25_PAD_VSYNC__VSYNC,
+	MX25_PAD_OE_ACD__OE_ACD,
+};
+
+/*
+	From DTS
+
+	hactive = <800>;
+	hfront-porch = <210>;
+	hback-porch = <52>;
+	hsync-len = <1>;
+	vactive = <480>;
+	vfront-porch = <22>;
+	vback-porch = <22>;
+	vsync-len = <1>;
+	clock-frequency = <40000000>;
+	hsync-active = <0>; // asserted when LOW
+	vsync-active = <0>; // asserted when LOW
+	de-active = <1>; // asserted when HIGH
+	pixelclk-active = <1>; // 1 = falling edge
+ */
+
+static struct fb_videomode openx32_fb_mode = {
+	.name = "Innolux-AT070TN83",
 	//.refresh	= 60,
+	.pixclock.ps = 33333,
 	.xres		= 800,
 	.yres		= 480,
 	
-	.right_margin	= 33, // hfront porch
-	.left_margin	= 47,  // hback porch / blanking
+	.right_margin	= 210, // hfront porch
+	.left_margin	= 45,  // hback porch / blanking
 	.hsync_len	= 1,
 	
-	.upper_margin	= 24,  // vback porch
-	.lower_margin	= 18,  // vfront porch
+	.upper_margin	= 22,  // vback porch
+	.lower_margin	= 22,  // vfront porch
 	.vsync_len	= 1,
 
-	//.vmode = FB_VMODE_NONINTERLACED,
-	//.sync = FB_SYNC_BROADCAST,
+	//  .display_flags = 	DISPLAY_FLAGS_HSYNC_LOW |
+	//  					DISPLAY_FLAGS_VSYNC_LOW |
+	//  					DISPLAY_FLAGS_DE_HIGH |
+	//  					DISPLAY_FLAGS_PIXDATA_NEGEDGE,
 
-	.display_flags = 	DISPLAY_FLAGS_HSYNC_LOW |
-						DISPLAY_FLAGS_VSYNC_LOW |
-						DISPLAY_FLAGS_DE_HIGH |
-						DISPLAY_FLAGS_PIXDATA_NEGEDGE,
+	//.sync = FB_SYNC_HOR_HIGH_ACT | FB_SYNC_VERT_HIGH_ACT,
+
+	//.vmode = FB_VMODE_NONINTERLACED,
+
+	// // //https://github.com/apc-io/apc-8750/blob/master/kernel/drivers/video/wmt/devices/lcd-INNOLUX-AT070TN83.c
+	// .right_margin	= 210, // hfront porch
+	// .left_margin	= 45,  // hback porch / blanking
+	// .hsync_len	= 1,
+	
+	// .upper_margin	= 22,  // vback porch
+	// .lower_margin	= 22,  // vfront porch
+	// .vsync_len	= 1,
+
 };
 
-static struct imx_fb_platform_data tx25_fb_data = {
-	.mode		= &stk5_fb_mode,
+static struct imx_fb_platform_data openx32_fb_data = {
+	.mode		= &openx32_fb_mode,
 	.num_modes	= 1,
 	.lscr1		= 0x00120300,
 	.dmacr		= 0x00020010,
 	.pcr		= 0xFC228080,
 	.bpp		= 32,	
-	.enable		= tx25_fb_enable,
+	.enable		= openx32_fb_enable,
 };
 
 static int openx32_init_fb(void)
@@ -168,48 +223,39 @@ static int openx32_init_fb(void)
 	}
 
 	// set LPCR-Register
-	writel(0xFC228080, 0x53FBC018);
+	//writel(0xFC228080, 0x53FBC018);
 
-	// set GPIOs within IOMUXC
-	static const iomux_v3_cfg_t gpio_pads[] = {
-		NEW_PAD_CTRL(MX25_PAD_CONTRAST__PWM4_PWMO, 0),
-		NEW_PAD_CTRL(MX25_PAD_PWM__GPIO_1_26, 0),
-		NEW_PAD_CTRL(MX25_PAD_D9__GPIO_4_11, 0),
-		NEW_PAD_CTRL(MX25_PAD_UART1_RTS__GPIO_4_24, 0),
-		NEW_PAD_CTRL(MX25_PAD_UART2_RTS__GPIO_4_28, 0),
-	};
-	mxc_iomux_v3_setup_multiple_pads(gpio_pads, ARRAY_SIZE(gpio_pads));
+	// // // set GPIOs within IOMUXC
+	// static const iomux_v3_cfg_t gpio_pads[] = {
+	// 	NEW_PAD_CTRL(MX25_PAD_CONTRAST__PWM4_PWMO, 0),
+	// // 	NEW_PAD_CTRL(MX25_PAD_PWM__GPIO_1_26, 0),
+	// // 	NEW_PAD_CTRL(MX25_PAD_D9__GPIO_4_11, 0),
+	// // 	NEW_PAD_CTRL(MX25_PAD_UART1_RTS__GPIO_4_24, 0),
+	// // 	NEW_PAD_CTRL(MX25_PAD_UART2_RTS__GPIO_4_28, 0),
+	// };
+	// mxc_iomux_v3_setup_multiple_pads(gpio_pads, ARRAY_SIZE(gpio_pads));
 
+	// ###############################################################
 	// enable display-backlight
-	// configure Prescaler but disable PWM (PWM_PWMC)
-	asm volatile(
-	"ldr r0, =0x53FC8000\n\t"
-	"ldr r1, =0x000100A0\n\t"
-	"str r1, [r0]"
-	);
+		
+	// // configure Prescaler but disable PWM (PWM_PWMC)
+	// writel(0x000100A0, 0x53FC8000);
 	// set periode (PWM_PWMPR) and dutycycle (PWM_PWMSAR)
-	asm volatile(
-	"ldr r0, =0x53FC8010\n\t"
-	"ldr r1, =0x00000190\n\t"
-	"str r1, [r0]\n\t"
-	"ldr r0, =0x53FC800C\n\t"
-	"ldr r1, =0x000000C8\n\t"
-	"str r1, [r0]"
-	);
-	// configure Prescaler and enable PWM (PWM_PWMC)
-	asm volatile(
-	"ldr r0, =0x53FC8000\n\t"
-	"ldr r1, =0x000100A1\n\t"
-	"str r1, [r0]"
-	);
+	writel(0x00000190, 0x53FC8010);
+	writel(0x000000C8, 0x53FC800C);
+	// // configure Prescaler but enable PWM (PWM_PWMC)
+	// writel(0x000100A0, 0x53FC8001);
+	
+	// ###############################################################
 
-	tx25_fb_enable(0);
+	openx32_fb_enable(0);
 
-	//fb_videomode_set_pixclock_hz(&stk5_fb_mode, 33300000);  // 33.3MHz
-	fb_videomode_set_pixclock_hz(&stk5_fb_mode, 40000000);  // 40MHz
-	add_generic_device("imxfb",	-1,	NULL, (resource_size_t)MX25_LCDC_BASE_ADDR, 0x1000, IORESOURCE_MEM, &tx25_fb_data);
+	// mxc_iomux_v3_setup_multiple_pads(openx32_lcdc_gpios,
+	// 		ARRAY_SIZE(openx32_lcdc_gpios));
 
-	fb_disable()
+	//fb_videomode_set_pixclock_hz(&openx32_fb_data, 40000000);  // 40MHz
+	fb_videomode_set_pixclock_hz(&openx32_fb_data, 33333333);  // 33.3MHz
+	add_generic_device("imxfb",	-1,	NULL, (resource_size_t)MX25_LCDC_BASE_ADDR, 0x2000, IORESOURCE_MEM, &openx32_fb_data);
 
 	return 0;
 }
