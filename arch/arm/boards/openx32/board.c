8571949ed8c493ce5b54b64b23bd4e3b84da21e6
Alexander Schulz <code@schulzalex.de>
2025-08-12 02:30:04 +0200

    Final basic working LCD setting (also after full  power reset). Still some artifacts - looks like some frequency is not right or hsync/vsync not correctly working.

--- arch/arm/boards/openx32/board.c
+++ arch/arm/boards/openx32/board.c
@@ -118,6 +118,31 @@ console_initcall(openx32_init);
 
 // ######################################################################
 
+static iomux_v3_cfg_t openx32_lcdc_gpios[] = {
+	MX25_PAD_LSCLK__LSCLK,
+	MX25_PAD_LD0__LD0,
+	MX25_PAD_LD1__LD1,
+	MX25_PAD_LD2__LD2,
+	MX25_PAD_LD3__LD3,
+	MX25_PAD_LD4__LD4,
+	MX25_PAD_LD5__LD5,
+	MX25_PAD_LD6__LD6,
+	MX25_PAD_LD7__LD7,
+	MX25_PAD_LD8__LD8,
+	MX25_PAD_LD9__LD9,
+	MX25_PAD_LD10__LD10,
+	MX25_PAD_LD11__LD11,
+	MX25_PAD_LD12__LD12,
+	MX25_PAD_LD13__LD13,
+	MX25_PAD_LD14__LD14,
+	MX25_PAD_LD15__LD15,
+	MX25_PAD_D15__LD16,
+	MX25_PAD_D14__LD17,
+	MX25_PAD_HSYNC__HSYNC,
+	MX25_PAD_VSYNC__VSYNC,
+	MX25_PAD_OE_ACD__OE_ACD,
+};
+
 
 static void openx32_fb_enable(int enable)
 {
@@ -131,7 +156,6 @@ static void openx32_fb_enable(int enable)
 static struct fb_videomode openx32_fb_mode = {
 	.name = "Innolux-AT070TN83",
 	.refresh	= 60,
-	//.pixclock.ps = 30000,  // 33.333333 MHz
 	.pixclock.ps = 25000,  // 40 MHz
 	.xres		= 800,
 	.yres		= 480,
@@ -148,7 +172,7 @@ static struct fb_videomode openx32_fb_mode = {
 static struct imx_fb_platform_data openx32_fb_data = {
 	.mode		= &openx32_fb_mode,
 	.num_modes	= 1,
-	.lscr1		= 0x00120300,
+	//.lscr1		= 0x00120300,
 	.dmacr		= 0x00020010,
 	.pcr		= 0xFC228080,
 	.bpp		= 32,	
@@ -167,21 +191,16 @@ static int openx32_init_fb(void)
 	};
 	mxc_iomux_v3_setup_multiple_pads(gpio_pads, ARRAY_SIZE(gpio_pads));
 
-	// ###############################################################
 	// enable display-backlight
-		
-	// // configure Prescaler but disable PWM (PWM_PWMC)
-	// writel(0x000100A0, 0x53FC8000);
 	// set periode (PWM_PWMPR) and dutycycle (PWM_PWMSAR)
 	writel(0x00000190, 0x53FC8010);
 	writel(0x000000C8, 0x53FC800C);
-	// // configure Prescaler but enable PWM (PWM_PWMC)
-	// writel(0x000100A0, 0x53FC8001);
-	
-	// ###############################################################
+
+	mxc_iomux_v3_setup_multiple_pads(openx32_lcdc_gpios,
+	 		ARRAY_SIZE(openx32_lcdc_gpios));
 
 	openx32_fb_enable(0);
-	add_generic_device("imxfb",	-1,	NULL, (resource_size_t)MX25_LCDC_BASE_ADDR, 0x2000, IORESOURCE_MEM, &openx32_fb_data);
+	add_generic_device("imxfb",	-1,	NULL, (resource_size_t)MX25_LCDC_BASE_ADDR, 0x1000, IORESOURCE_MEM, &openx32_fb_data);
 
 	return 0;
 }
