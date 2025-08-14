fab0558cd925a41c1b3d03d3f9ff8c5fdedc6d19
Alexander Schulz <code@schulzalex.de>
2025-08-15 00:39:14 +0200

    workaround for debug_ll

--- include/debug_ll/pl011.h
+++ include/debug_ll/pl011.h
@@ -10,18 +10,18 @@
 #include <io.h>
 #include <linux/amba/serial.h>
 
-static inline void PUTC_LL(char c)
-{
-	/* Wait until there is space in the FIFO */
-	while (readl(DEBUG_LL_UART_ADDR + UART01x_FR) & UART01x_FR_TXFF)
-		;
+// static inline void PUTC_LL(char c)
+// {
+// 	/* Wait until there is space in the FIFO */
+// 	while (readl(DEBUG_LL_UART_ADDR + UART01x_FR) & UART01x_FR_TXFF)
+// 		;
 
-	/* Send the character */
-	writel(c, DEBUG_LL_UART_ADDR + UART01x_DR);
+// 	/* Send the character */
+// 	writel(c, DEBUG_LL_UART_ADDR + UART01x_DR);
 
-	/* Wait to make sure it hits the line, in case we die too soon. */
-	while (readl(DEBUG_LL_UART_ADDR + UART01x_FR) & UART01x_FR_TXFF)
-		;
-}
+// 	/* Wait to make sure it hits the line, in case we die too soon. */
+// 	while (readl(DEBUG_LL_UART_ADDR + UART01x_FR) & UART01x_FR_TXFF)
+// 		;
+// }
 
 #endif /* __INCLUDE_ARM_ASM_DEBUG_LL_PL011_H__ */
